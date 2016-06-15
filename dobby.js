3084bafd2006fe1125b9704c371bb7c05e6beab9
Amit Bhadoria <bhadoria@cisco.com>
2016-06-15 01:40:37 -0700

    integrated dobby app with spark message

--- dobby.js
+++ dobby.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const request = require('request');
+const sleep = require('sleep');
 const Wit = require('node-wit').Wit;
 // When cloning the `node-wit` repo, replace the `require` like so:
 // const Wit = require('../').Wit;
@@ -44,11 +46,99 @@ const bestEntityValue = (entities, entity) => {
 
 const token = "RO7ASNPUVWRS6JVSSWAQ534IMGHETEBN";
 
-var state = {};
+const pollReq = request.defaults({
+  uri: 'https://dobby-spark.appspot.com/v1/poll/amit1on1',
+  method: 'GET',
+  headers: {'Content-Type': 'application/json'},
+});
+
+const getMessages = (cb) => {
+  // console.log('polling for messages...');
+  const opts = {};
+  request.get('https://dobby-spark.appspot.com/v1/poll/amit1on1', (err, resp, data) => {
+  // console.log('finished polling');
+    if (cb) {
+      cb(err || data.error && data.error.message, data);
+    }
+  });
+};
+
+const postReq = request.defaults({
+  uri: 'https://api.ciscospark.com/v1/messages',
+  method: 'POST',
+  headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer MTFkN2UwZDQtN2RjYi00NmQ0LWJkZjQtZDcwNjk1MjBkM2VmOThkZDdmODgtZTgx'},
+});
+
+const sendMessage = (roomId, text, cb) => {
+  const opts = {
+    json: {
+      roomId: roomId,
+      text: text,
+    },
+  };
+  postReq(opts, (err, resp, data) => {
+    if (cb) {
+      cb(err || data.error && data.error.message, data);
+    }
+  });
+};
+
+const getReq = request.defaults({
+  // uri: 'https://api.ciscospark.com/v1/messages/',
+  method: 'GET',
+  headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer MTFkN2UwZDQtN2RjYi00NmQ0LWJkZjQtZDcwNjk1MjBkM2VmOThkZDdmODgtZTgx'},
+});
+
+const fetchMessage = (msgId, cb) => {
+  // console.log("fetching message:", msgId);
+  getReq('https://api.ciscospark.com/v1/messages/' + msgId, (err, resp, data) => {
+    // console.log("fetched message:", data);
+    if (cb) {
+      cb(err || data.error && data.error.message, data);
+    }
+  });
+};
+
+const sessions = {};
+
+const findOrCreateSession = (roomId) => {
+  let sessionId;
+  // Let's see if we already have a session for the roomId
+  Object.keys(sessions).forEach(k => {
+    if (sessions[k].roomId === roomId) {
+      // Yep, got it!
+      sessionId = k;
+    }
+  });
+  if (!sessionId) {
+    // No session found for user fbid, let's create a new one
+    sessionId = new Date().toISOString();
+    sessions[sessionId] = {roomId: roomId, context: {}};
+  }
+  return sessionId;
+};
+
 
 const actions = {
   say(sessionId, context, message, cb) {
     console.log(message);
+    // send message to spark room
+    const roomId = sessions[sessionId].roomId;
+    if (roomId) {
+      // we have a room for this sesssion, send message there
+      sendMessage(roomId, message, (err, data) => {
+        if (err) {
+          console.log(
+            'Oops! An error occurred while forwarding the response to',
+            roomId,
+            ':',
+            err
+          );
+        }
+      });
+    } else {
+      console.log("did not find any room Id");
+    }
     cb();
   },
   merge(sessionId, context, entities, message, cb) {
@@ -89,5 +179,62 @@ const actions = {
   },
 };
 
-const client = new Wit(token, actions);
-client.interactive();
+const wit = new Wit(token, actions);
+const poll = (err, d) => {
+    if (err) {
+      console.log(
+        'Oops! An error occurred while fetching messages:',
+        err
+      );
+    } else {
+      // console.log("got messages:", d);
+      // walk through each message notification
+      if (d) {
+        var data = JSON.parse(d);
+        data.forEach(function(value) {
+            if (value.data.personEmail && value.data.personEmail.includes('dobby.spark@gmail.com')) {
+              // this is my own message, so discard
+              console.log("discarding my own message");
+            } else {
+            // fetch the message from spark
+            fetchMessage(value.data.id, (err, d) => {
+              if (d) {
+                console.log("got message:", d);
+                var data = JSON.parse(d);
+                const sessionId = findOrCreateSession(data.roomId);
+                wit.runActions(
+                  sessionId, // the user's current session
+                  data['text'], // the user's message 
+                  sessions[sessionId].context, // the user's current session state
+                  (error, context) => {
+                    if (error) {
+                      console.log('Oops! Got an error from Wit:', error);
+                    } else {
+                      // Our bot did everything it has to do.
+                      // Now it's waiting for further messages to proceed.
+                      console.log('Waiting for further messages.');
+
+                      // Based on the session state, you might want to reset the session.
+                      // This depends heavily on the business logic of your bot.
+                      // Example:
+                      // if (context['done']) {
+                      //   delete sessions[sessionId];
+                      // }
+
+                      // Updating the user's current session state
+                      sessions[sessionId].context = context;
+                    }
+                  }
+                );
+              }
+            });              
+            }
+        });
+      }
+    }
+    // sleep
+    sleep.sleep(1);
+    getMessages(poll);
+};
+  
+getMessages(poll);
