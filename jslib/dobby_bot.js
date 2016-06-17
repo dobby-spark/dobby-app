c383918fa27e495670edd749b808c0841f4722ab
Amit Bhadoria <bhadoria@cisco.com>
2016-06-16 22:27:16 -0700

    switched dobby vocab to use cassandra

--- jslib/dobby_bot.js
+++ jslib/dobby_bot.js
@@ -1,17 +1,18 @@
 /* dobby_spark.js */
 'use strict';
 
-const parser = require('./dobby_parse');
+const parser = require('./dobby_parse_cass');
 
 module.exports = {
   runActions: runActions,
 };
 
 function runActions(actions, sessionId, message, context, cb) {
-  actions.merge(sessionId, context, parser.parseMessage(message), message, (arg) => {
-    // no op
-  });
-  actions.nextState(sessionId, {}, (arg, ctx) => {
-    // no op
-  });
+	parser.parseMessage(message, (res) => {
+		actions.merge(sessionId, context, res, message, (arg) => {
+			actions.nextState(sessionId, {}, (arg, ctx) => {
+			// no op
+			});
+		})
+	});  
 };
--- jslib/dobby_cass.js
+++ jslib/dobby_cass.js
@@ -13,7 +13,7 @@ const cassClient = new cassandra.Client({ contactPoints: ['ucm211.cisco.com'], k
 function getState(intent, topic, state, input, cb) {
   var query = 'SELECT msg, nextState, nextIntent FROM state_mc WHERE intent=? AND topic=? AND state=? AND input=?';
   var params = [intent, topic, state, input];
-  console.log("Cass read params:", params);
+  // console.log("Cass read params:", params);
   cassClient.execute(query, params, cb);
   //  cassClient.execute(query, params, function (err, result) {
   //   if (err) {
@@ -34,13 +34,13 @@ function getVocab(type, cb) {
   var query = 'SELECT key, result FROM vocab WHERE type=?';
   // var query = "SELECT key, result FROM vocab WHERE type='input'";
   var params = [type];
-  console.log("Cass read params:", params);
+  // console.log("Cass read params:", params);
   cassClient.execute(query, params, cb);
 }
 
 function getVocabResult(type, key, cb) {
   var query = 'SELECT result FROM vocab WHERE type=? AND key=?';
   var params = [type, key];
-  console.log("Cass read params:", params);
+  // console.log("Cass read params:", params);
   cassClient.execute(query, params, cb);
 }
